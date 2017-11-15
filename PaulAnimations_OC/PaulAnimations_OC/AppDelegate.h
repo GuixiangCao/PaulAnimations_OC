5895e96943c3b6141d08201bc19a4f30499f505d
paul <paul09@126.com>
2017-11-15 16:19:27 +0800

    -init Project

---
+++ PaulAnimations_OC/PaulAnimations_OC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PaulAnimations_OC
+//
+//  Created by paul on 2017/11/15.
+//  Copyright © 2017年 paul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PaulAnimations_OC/PaulAnimations_OC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PaulAnimations_OC
+//
+//  Created by paul on 2017/11/15.
+//  Copyright © 2017年 paul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
